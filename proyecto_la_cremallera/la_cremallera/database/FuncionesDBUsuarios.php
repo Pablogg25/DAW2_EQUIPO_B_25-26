e3b07e2be8e96f506db82b0f3dc74e0178fbb3ae
DAW2DESARROLLO\usuario <gustavorodrigo.bautistapocohuanca@iesmiguelherrero.es>
2025-12-17 16:50:35 +0100

    subiendo la funcion getUsuarios

--- proyecto_la_cremallera/la_cremallera/database/FuncionesDBUsuarios.php
+++ proyecto_la_cremallera/la_cremallera/database/FuncionesDBUsuarios.php
@@ -1,11 +1,22 @@
 <?php
 
-namespace la_cremallera\database;
+use la_cremallera\database\ConexionBD;
+
+require_once __DIR__ . '/ConexionDB.php';
 
 final class FuncionesDB{
 
     final public static function getUsuarios(){
         
+        $establecerConexionBD = ConexionBD::getConnection();
+
+        $comandoSql = "SELECT * FROM usuarios";
+
+        $stmt = $establecerConexionBD -> prepare($comandoSql);
+        $stmt -> execute();
+
+        return $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
     }
 
 }
