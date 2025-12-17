bc84c7a1ee3749d817574ba9cf4ac7bb4b2f5952
DAW2DESARROLLO\usuario <pablonunezsanchez28@gmail.com>
2025-12-17 16:20:17 +0100

    función getUsuarios nombre email PabloNuñez

--- proyecto_la_cremallera/la_cremallera/database/FuncionesDBUsuarios.php
+++ proyecto_la_cremallera/la_cremallera/database/FuncionesDBUsuarios.php
@@ -2,10 +2,22 @@
 
 namespace la_cremallera\database;
 
+use PDO;
+
 final class FuncionesDB{
 
-    final public static function getUsuarios(){
-        
+    final public static function getUsuarios(PDO $conexion){
+        //obtener todos los usuarios de la base de datos
+
+        if(isset($conexion)){
+            echo "<p>ERROR CONEXION BD: conexión no establecida</p>";
+            return;
+        }
+        $q_selectUsuarios="SELECT nombre,email FROM usuarios";
+
+        $resultadoQuery=$conexion->query($q_selectUsuarios);
+
+        return $resultadoQuery;
     }
 
 }
