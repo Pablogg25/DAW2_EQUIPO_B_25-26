0767d6943f61f2133d55f95b9eee6f44eae99862
DAW204\usuario <pablonunezsanchez28@gmail.com>
2025-12-18 19:05:41 +0100

    Añadido CRUD para asociación de trabajo_factura

--- proyecto_la_cremallera/la_cremallera/database/FuncionesDBFacturas.php
+++ proyecto_la_cremallera/la_cremallera/database/FuncionesDBFacturas.php
@@ -111,10 +111,11 @@ final class FuncionesDBFacturas
      * - FuncionesDBException
      * - PDOException
      */
-    final public static function getItemsFactura($args){
-        $q_selectItems="SELECT * FROM trabajos t RIGHT JOIN factura_trabajo ft on t.trabajoId=f.facturaId WHERE facturaId = :id";
+    final public static function getItemsFactura($args)
+    {
+        $q_selectItems = "SELECT * FROM trabajos t RIGHT JOIN factura_trabajo ft on t.trabajoId=ft.trabajoId WHERE ft.facturaId = :id";
 
-        $facturaId=$args['facturaId'] ?? -1;
+        $facturaId = $args['facturaId'] ?? -1;
 
         if ($facturaId < 0 || gettype($facturaId) != 'integer') {
             throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): valor de facturaId no reconocido");
@@ -129,6 +130,72 @@ final class FuncionesDBFacturas
         $stmt->execute([":id" => $facturaId]);
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * getFacturasByTrabajoId($args)
+     * Obtiene los datos de las facturas asociadas a un trabajo indicado por trabajoId
+     * 
+     * $args:
+     * - trabajoId (requerido,FK)
+     * 
+     * Excepciones:
+     * - FuncionesDBException
+     * - PDOException
+     */
+    public static function getFacturasByTrabajoId($args)
+    {
+        $q_selectItems = "SELECT * FROM facturas f RIGHT JOIN factura_trabajo ft on f.facturaId=ft.facturaId WHERE ft.trabajoId = :id";
+
+        $trabajoId = $args['trabajoId'] ?? -1;
+
+        if ($trabajoId < 0 || gettype($trabajoId) != 'integer') {
+            throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): valor de trabajoId no reconocido");
+        }
+
+        $conexion = ConexionBD::getConnection();
+        if (!isset($conexion)) {
+            throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): no se ha podido establecer conexion BBDD");
+        }
+
+        $stmt = $conexion->prepare($q_selectItems);
+        $stmt->execute([":id" => $trabajoId]);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * getTotalFactura($args)
+     * calcula la suma total de los items asociados a una factura en base a la suma total de los trabajos asociados.
+     * 
+     * $args:
+     * - facturaId (requerido)
+     * 
+     * Excepciones:
+     * - FuncionesDBException
+     * - PDOException
+     */
+    final public static function getTotalFactura($args)
+    {
+        $q_totalFacturaItems = "SELECT sum(t.precio) as total FROM trabajos t RIGHT JOIN factura_trabajo ft ON t.trabajoId=ft.trabajoId WHERE ft.facturaId= :id";
+
+        $facturaId = $args['facturaId'] ?? -1;
+
+        if ($facturaId < 0 || gettype($facturaId) != 'integer') {
+            throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): valor de facturaId no reconocido");
+        }
+
+        $conexion = ConexionBD::getConnection();
+        if (!isset($conexion)) {
+            throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): no se ha podido establecer conexion BBDD");
+        }
+
+        $stmt = $conexion->prepare($q_totalFacturaItems);
+        $stmt->execute([":id" => $facturaId]);
+
+        $result = $stmt->fetch(PDO::FETCH_COLUMN);
+
+        return $result['total']??0;
     }
 
     // ---CREATE---
@@ -189,11 +256,12 @@ final class FuncionesDBFacturas
      * - FuncionesDBException
      * - PDOException
      */
-    final public static function asociarFacturaTrabajo($args){
-        $q_insertFacturaTrabajo="INSERT INTO factura_trabajo (factura_id,trabajo_id) VALUES (:fid,:tid)";
+    final public static function asociarFacturaTrabajo($args)
+    {
+        $q_insertFacturaTrabajo = "INSERT INTO factura_trabajo (factura_id,trabajo_id) VALUES (:fid,:tid)";
 
-        $facturaId=$args['facturaId']??-1;
-        $trabajoId=$args['trabajoId']??-1;
+        $facturaId = $args['facturaId'] ?? -1;
+        $trabajoId = $args['trabajoId'] ?? -1;
 
         if ($facturaId < 0 || gettype($facturaId) != 'integer') {
             throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): valor facturaId no reconocido ");
@@ -235,15 +303,16 @@ final class FuncionesDBFacturas
      * - FuncionesDBException
      * - PDOException
      */
-    final public static function updateFactura($args){
-        $q_updateFactura="UPDATE facturas SET usuarioId = :usuarioId, fecha= :fecha, pagado= :pagado, total_calculado = :tc WHERE facturaId = :id";
+    final public static function updateFactura($args)
+    {
+        $q_updateFactura = "UPDATE facturas SET usuarioId = :usuarioId, fecha= :fecha, pagado= :pagado, total_calculado = :tc WHERE facturaId = :id";
 
-        $facturaId=$args['facturaId']??-1;
+        $facturaId = $args['facturaId'] ?? -1;
 
-        $usuarioId=$args['usuarioId']??-1;
-        $fecha=$args['fecha']??'';
-        $pagado=$args['pagado']??0;
-        $totalCalculado=$args['total_calculado']??'null';
+        $usuarioId = $args['usuarioId'] ?? -1;
+        $fecha = $args['fecha'] ?? '';
+        $pagado = $args['pagado'] ?? 0;
+        $totalCalculado = $args['total_calculado'] ?? 'null';
 
         if ($facturaId < 0 || gettype($facturaId) != 'integer') {
             throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): valor de facturaId no reconocido");
@@ -253,7 +322,7 @@ final class FuncionesDBFacturas
             throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): valor de usuarioId no reconocido");
         }
 
-        if($fecha==''){
+        if ($fecha == '') {
             throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): el campo fecha es requerido");
         }
 
@@ -265,11 +334,11 @@ final class FuncionesDBFacturas
         $stmn = $conexion->prepare($q_updateFactura);
 
         $success = $stmn->execute([
-            ':id'=>$facturaId,
+            ':id' => $facturaId,
             ':usuarioId' => $usuarioId,
             ':fecha' => $fecha,
-            ':pagado'=>$pagado,
-            ':total_calculado'=>$totalCalculado
+            ':pagado' => $pagado,
+            ':total_calculado' => $totalCalculado
         ]);
 
         return $success;
@@ -288,11 +357,12 @@ final class FuncionesDBFacturas
      * - FuncionesDBException
      * - PDOException
      */
-    final public static function deleteFactura($args){
-        $q_deleteFactura="DELETE FROM facturas WHERE facturaId = :id";
+    final public static function deleteFactura($args)
+    {
+        $q_deleteFactura = "DELETE FROM facturas WHERE facturaId = :id";
 
-        $facturaId=$args['facturaId']??-1;
-        
+        $facturaId = $args['facturaId'] ?? -1;
+
         if ($facturaId < 0 || gettype($facturaId) != 'integer') {
             throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): valor de facturaId no reconocido");
         }
@@ -305,13 +375,51 @@ final class FuncionesDBFacturas
         $stmn = $conexion->prepare($q_deleteFactura);
 
         $success = $stmn->execute([
-            ':id'=>$facturaId,
+            ':id' => $facturaId,
         ]);
 
         return $success;
     }
 
-    final public static function desasociarFacturaTrabajo($args){
+    /**
+     * desasociarFacturaTrabajo($args)
+     * Elimina la asociación en la tabla factura_trabajo de un item a una factura pasando sus pares de ids
+     * 
+     * $args:
+     * - facturaId (requerido,FK)
+     * - trabajoId (requerido,FK)
+     * 
+     * Excepciones:
+     * - FuncionesDBException
+     * - PDOException
+     */
+    final public static function desasociarFacturaTrabajo($args)
+    {
+        $q_deleteFacturaTrabajo = "DELETE FROM factura_trabajo WHERE facturaId = :fid AND trabajoId = :tid";
 
+        $facturaId = $args['facturaId'] ?? -1;
+        $trabajoId = $args['trabajoId'] ?? -1;
+
+        if ($facturaId < 0 || gettype($facturaId) != 'integer') {
+            throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): valor facturaId no reconocido ");
+        }
+
+        if ($trabajoId < 0 || gettype($trabajoId) != 'integer') {
+            throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): valor de trabajoId no reconocido");
+        }
+
+        $conexion = ConexionBD::getConnection();
+        if (!isset($conexion)) {
+            throw new FuncionesDBException("ERROR FUNCIONES BD (FACTURAS): no se ha podido establecer conexion BBDD");
+        }
+
+        $stmn = $conexion->prepare($q_deleteFacturaTrabajo);
+
+        $success = $stmn->execute([
+            ':fid' => $facturaId,
+            ':tid' => $trabajoId
+        ]);
+
+        return $success;
     }
 }
