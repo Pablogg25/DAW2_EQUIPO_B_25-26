0756fcc1639d5db0d322f4a333763b94b1c0417c
PnunezS28 <pablonunezsanchez28@gmail.com>
2026-02-09 20:10:04 +0100

    Primer maquetado de formulario de create/update orders

--- Frontend/LaCremallera/src/core/TestController/TestOrdersController.js
+++ Frontend/LaCremallera/src/core/TestController/TestOrdersController.js
@@ -8,7 +8,7 @@ const $ordersController = (function () {
     }
     let trabajos = JSON.parse(localStorage.getItem('trabajos'));
 
-    function siguientePacienteId() {
+    function siguienteOrderId() {
         let maxId = Math.max(...trabajos.map(p => p.trabajoId), 0);
         return maxId + 1;
     }
@@ -16,24 +16,55 @@ const $ordersController = (function () {
     async function getOrders(params) {
         console.log("AVISO: ESTE ES UN MÉTODO DE PRUEBAS, NO TIENE CONEXIÓN REAL CON LA API");
         console.log("ordersController: getOrders");
-        
+
         return [...trabajos];
     }
-    async function obtenerOrder(trabajoId){
+    async function getOrder(trabajoId) {
         console.log("AVISO: ESTE ES UN MÉTODO DE PRUEBAS, NO TIENE CONEXIÓN REAL CON LA API");
-        console.log("ordersController: obtenerOrder: "+trabajoId);
-        let index=trabajos.findIndex(p=>p.trabajoId==trabajoId);
+        console.log("ordersController: obtenerOrder: " + trabajoId);
+        let index = trabajos.findIndex(p => p.trabajoId == trabajoId);
 
-        if(index!==-1){
+        if (index !== -1) {
             return trabajos;
         }
     }
 
+    async function createOrder(objOrder) {
+        objOrder.id = siguienteOrderId();
+        trabajos.push(objOrder);
+        localStorage.setItem('trabajos', JSON.stringify(trabajos));
+
+        return objOrder.id;
+    }
+
+    async function updateOrder(objOrder) {
+        let index = trabajos.findIndex(u => u.id == objOrder.id);
+        if (index !== -1) {
+            trabajos[index] = objOrder;
+            localStorage.setItem('trabajos', JSON.stringify(trabajos));
+            return true;
+        }
+        return false;
+    }
+
+    async function deleteOrder(orderId) {
+        let index = trabajos.findIndex(u => u.id == orderId);
+        if (index !== -1) {
+            trabajos.splice(index, 1);
+            localStorage.setItem('trabajos', JSON.stringify(trabajos));
+            return true;
+        }
+        return false;
+    }
+
     return {
         getOrders,
-        obtenerOrder,
+        getOrder,
+        createOrder,
+        updateOrder,
+        deleteOrder
     }
 })();
 
-window.$ordersController=$ordersController;
+window.$ordersController = $ordersController;
 export default $ordersController;
---
+++ Frontend/LaCremallera/src/pages/Orders/OrderFormPage.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import $ordersController from "../../core/TestController/TestOrdersController";
+
+function OrderFormPage() {
+    const [orderData,setOrderData]=useState({
+        descripcion:"",empleado:0,estado:"",
+        fecha_entrega:"",fecha_inicio:"",
+        precio:0,prenda:"",trabajo_id:0
+    });
+
+    const navegar=useNavigate;
+
+    const handleOnSubmit=(evento)=>{
+        evento.preventDefault();
+
+        const response=$ordersController.createOrder(orderData);
+    }
+
+    const handleOnCancel=()=>{
+        navegar("/orders");
+    }
+
+    //metodo para adaptar formulario
+    const handleOnChange=(evento)=>{
+        const {name,value}=evento.target;
+        let actualizar={...orderData,[name]:value};
+        setOrderData(actualizar);
+    }
+
+    return (
+        <div>
+            <div>Formulario create</div>
+
+            <form>
+                <div>
+                    Formulario de datos Order
+                </div>
+                {/* formulario */}
+                <div>
+                    <div> Descripción: </div>
+                    <input type="text" name="descripcion" id="descripcion" onChange={handleOnChange}/>
+                </div>
+                <div>
+                    <div> Prenda: </div>
+                    <input type="text" name="prenda" id="prenda" onChange={handleOnChange} />
+                </div>
+                <div>
+                    <div> Empleado: </div>
+                    {/* drop down con los empleados disponibles */}
+                    <select name="empleado" id="empleado" onChange={handleOnChange}>
+                        <option value="1">empleado 1</option>
+                        <option value="2">empleado 2</option>
+                        <option value="3">empleado 3</option>
+                    </select>
+                </div>
+                <div>
+                    <div> fecha de inicio: </div>
+                    <input type="date" name="fecha_inicio" id="fecha_inicio" onChange={handleOnChange} />
+                </div>
+                <div>
+                    <div> fecha de entrega: </div>
+                    <input type="date" name="fecha_entrega" id="fecha_entrega" onChange={handleOnChange} />
+                </div>
+                <div>
+                    <div> precio: </div>
+                    <input type="number" name="precio" id="precio" onChange={handleOnChange} />
+                </div>
+                <div>
+                    <div> estado: </div>
+                    <select name="estado" id="estado" onChange={handleOnChange}>
+                        <option value="pendiente">pendiente</option>
+                        <option value="en_proceso">En proceso</option>
+                        <option value="listo">listo</option>
+                        <option value="entregado">entregado</option>
+                    </select>
+                </div>
+
+                <div>
+                    <button type="submit">Enviar datos</button>
+                    <button>Cancelar</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default OrderFormPage;
