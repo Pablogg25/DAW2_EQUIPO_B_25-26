45d9f0b0e9ec55f45ed5034dd37c7bd54887d25f
PnunezS28 <pablonunezsanchez28@gmail.com>
2026-02-09 18:11:23 +0100

    test implement primera estructura

--- Frontend/LaCremallera/src/router/AppEnrutador.jsx
+++ Frontend/LaCremallera/src/router/AppEnrutador.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "../components/AppLayout";
 import HomePage from "../pages/HomePage";
+import OrdersPage from "../pages/OrdersPage.jsx";
 
 import ErrorPage from "../pages/ErrorPage.jsx";
 function AppEnrutador() {
@@ -9,6 +10,7 @@ function AppEnrutador() {
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route index element={<HomePage />} />
+          <Route path="orders" element={<OrdersPage />} />
 
           <Route path="*" element={<ErrorPage />} />
         </Route>
