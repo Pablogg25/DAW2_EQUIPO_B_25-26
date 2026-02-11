41fe1884862ed15b6c405b8639208b8066a6c43a
WIN10DEV\pnunezs2401 <pablonunezsanchez28@gmail.com>
2026-02-11 18:13:07 +0100

    Primer maquetado de pagina login TODO: hacer context para poder guardar datos de usuario y permisos

--- Frontend/LaCremallera/src/pages/LoginPage.jsx
+++ Frontend/LaCremallera/src/pages/LoginPage.jsx
@@ -1,6 +1,87 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import $usersController from "../../core/UsersController";
+
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const [userCred,setUserCred]=useState({
+    username:"",password:""
+  });
+
+
+  const navegar=useNavigate();
+
+
+  const handleOnSubmit=(event)=>{
+    event.preventDefault();
+    enviarDatos();
+  }
+
+  const enviarDatos=async()=>{
+    console.log("Enviar datos");
+
+    const response=await $usersController.loginUser(userCred);
+      console.log(response);
+
+    if(response.success){
+      console.log("Datos recividos");
+      if(response.data){
+        alert("login correcto");
+
+        //TODO: insertar información del rol y permisos de usuario 
+        // en context
+        // TODO: hacer context
+
+        navegar("/");
+      }else{
+        alert("Credenciales incorrectas");
+      }
+    }
+
+  }
+
+  const handleOnChange = (evento) => {
+        const { name, value } = evento.target;
+        let actualizar = { ...userCred, [name]: value };
+        setUserCred(actualizar);
+    }
+
+  const handleOnCancel=(event)=>{
+    //TODO: navegar a homepage
+    event.preventDefault();
+    navegar("/");
+  }
+
+
+  return (<div>
+    <div>
+      Página Login
+    </div>
+
+    <form onSubmit={()=>{handleOnSubmit();}}>
+      <div>
+        <div>
+          Nombre de usuario:
+        </div>
+        <input type="text" name="username" id="username" onChange={handleOnChange}/>
+      </div>
+
+      <div>
+        <div>
+          password:
+        </div>
+        <input type="password" name="password" id="password" onChange={handleOnChange}/>
+      </div>
+
+      <div>
+        <button type="submit">Conectarse</button>
+        <button onClick={()=>{handleOnCancel();}}>Cancelar</button>
+      </div>
+      
+    </form>
+
+  </div>);
 }
 export default LoginPage;
