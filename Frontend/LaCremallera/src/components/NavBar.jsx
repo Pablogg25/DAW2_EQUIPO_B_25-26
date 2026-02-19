3c84935190a6b795b5b096f53c71d39604f889eb
Pablo <pgonzalezg12@educantabria.es>
2026-02-19 13:16:10 +0100

    Agregar la barra de navegación y los íconos de las aplicaciones
    Se introdujo un nuevo componente NavBar y se incluyó en el diseño de la aplicación; se actualizó el favicon y se agregaron recursos de iconos. index.html ahora usa /icono-app-1.png como favicon. Se añadieron tres imágenes de iconos al público (icono-app-1.png, icono-app-2.jpeg, icono-app-3(sin fondo).png). AppLayout.jsx importa y renderiza la nueva NavBar (basada en Bootstrap, con rutas NavLink para Inventario, Trabajos, Prendas, Usuarios y Notificaciones).

---
+++ Frontend/LaCremallera/src/components/NavBar.jsx
@@ -0,0 +1,66 @@
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+      {/* ICONO A LA IZQUIERDA */}
+      <NavLink className="navbar-brand d-flex align-items-center" to="/">
+        <img
+          src="../../public/icono-app-3(sin fondo).png"
+          alt="logo"
+          width="32"
+          height="32"
+          className="me-2"
+        />
+        La Cremallera
+      </NavLink>
+
+      {/* BOTÓN HAMBURGUESA */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#mainNavbar"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      {/* MENÚ */}
+      <div className="collapse navbar-collapse" id="mainNavbar">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <NavLink to="/inventory" className="nav-link">
+              Inventario
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink to="/orders" className="nav-link">
+              Trabajos
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink to="/prendas" className="nav-link">
+              Prendas
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink to="/users" className="nav-link">
+              Usuarios
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink to="/notificaciones" className="nav-link">
+              Notificaciones
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
