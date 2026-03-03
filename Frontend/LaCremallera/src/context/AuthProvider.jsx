6e529dc53c804e98e364d4ea1e0a203633a0e907
PnunezS28 <pablonunezsanchez28@gmail.com>
2026-03-03 16:06:06 +0100

    guardado auth token en session storage

--- Frontend/LaCremallera/src/context/AuthProvider.jsx
+++ Frontend/LaCremallera/src/context/AuthProvider.jsx
@@ -3,19 +3,27 @@ import { AuthContext } from "./AuthContext";
 
 function AuthProvider({ children }) {
   const [usuario, setUsuario] = useState(null);
-  const [token, setToken] = useState(null);
+  // const [token, setToken] = useState(null);
 
   function login(userData) {
     // console.log("Auth provider, saving data: ");
     // console.log(userData);
     setUsuario(userData.user);
-    setToken(userData.token)
+    // setToken(userData.token);
+    sessionStorage.setItem("authToken",userData.token);
   }
 
   function logout() {
     setUsuario(null);
-    setToken(null);
+    // setToken(null);
+    sessionStorage.setItem("authToken",null);
+
   }
+
+  function token(){
+    return sessionStorage.getItem("authToken");
+  }
+
 
   return (
     <AuthContext.Provider value={{ usuario, token, login, logout }}>
